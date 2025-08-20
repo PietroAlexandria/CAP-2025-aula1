3db3b3a049b03ed148279a7ad485d7bcf332b46d
Pietro <162758840+PietroAlexandria@users.noreply.github.com>
2025-08-20 01:42:06 +0000

    Aula-19-08-2025

--- src/main/java/app/PrimeiraClasse.java
+++ src/main/java/app/PrimeiraClasse.java
@@ -3,5 +3,12 @@ package app;
 public class PrimeiraClasse {
     public static void main(String[] args){
         System.out.println("Hello Gradle!!!");
+
+        Aluno a1 = new Aluno();
+        a1.setId(1);
+        a1.setNome("Pietro");
+        a1.setIdade(19);
+
+        System.out.println(a1.getNome());
     }
 }
