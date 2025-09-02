baebe083674e5f1d100dede0e398bd672615c950
Pietro <162758840+PietroAlexandria@users.noreply.github.com>
2025-09-02 23:28:55 +0000

    Aula-Passada-dados

--- src/main/java/app/Aluno.java
+++ src/main/java/app/Aluno.java
@@ -1,35 +1,86 @@
 package app;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
+import lombok.Generated;
 
 public class Aluno {
-    private long id;
-    private String nome;
-    private int idade;
+   private long id;
+   private String nome;
+   private int idade;
+   private String endereco;
+   private String curso;
+   private String telefone;
+   private String dataNascimento;
 
-    public long getId(){
-        return id;
-    }
-    public void setId(long id){
-        this.id = id;
-    }
+   public Aluno() {
+   }
 
-    public String getNome(){
-        return nome;
-    }
-    public void setNome(String nome){
-        this.nome = nome;
-    }
+   @Generated
+   public long getId() {
+      return this.id;
+   }
 
-    public int getIdade(){
-        return idade;
-    }
-    public void setIdade(int idade){
-        this.idade = idade;
-    }
+   @Generated
+   public String getNome() {
+      return this.nome;
+   }
 
+   @Generated
+   public int getIdade() {
+      return this.idade;
+   }
+
+   @Generated
+   public void setId(long id) {
+      this.id = id;
+   }
+
+   @Generated
+   public void setNome(String nome) {
+      this.nome = nome;
+   }
+
+   @Generated
+   public void setIdade(int idade) {
+      this.idade = idade;
+   }
+
+   @Generated
+   public String getEndereco() {
+      return endereco;
+   }
+
+   @Generated
+   public void setEndereco(String endereco) {
+      this.endereco = endereco;
+   }
+
+   @Generated
+   public String getCurso() {
+      return curso;
+   }
+
+   @Generated
+   public void setCurso(String curso) {
+      this.curso = curso;
+   }
+
+   @Generated
+   public String getTelefone() {
+      return telefone;
+   }
+
+   @Generated
+   public void setTelefone(String telefone) {
+      this.telefone = telefone;
+   }
+
+   @Generated
+   public String getDataNascimento() {
+      return dataNascimento;
+   }
+
+   @Generated
+   public void setDataNascimento(String dataNascimento) {
+      this.dataNascimento = dataNascimento;
+   }
 }
--- src/main/java/app/PrimeiraClasse.java
+++ src/main/java/app/PrimeiraClasse.java
@@ -1,14 +1,43 @@
 package app;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class PrimeiraClasse {
-    public static void main(String[] args){
-        System.out.println("Hello Gradle!!!");
+   public PrimeiraClasse() {
+   }
 
-        Aluno a1 = new Aluno();
-        a1.setId(1);
-        a1.setNome("Pietro");
-        a1.setIdade(19);
+   public static void main(String[] args) {
 
-        System.out.println(a1.getNome());
-    }
+      System.out.println(" -- Informações Aluno Unifio -- ");
+
+      LocalDateTime agora = LocalDateTime.now();
+      Long id = Long.valueOf(agora.format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss")));
+
+      Aluno a1 = new Aluno();
+      String nome = "Gustavo";
+      int idade = 21;
+      String endereco = "Rua Tirusbangos, Centro";
+      String curso = "Engenharia de Software";
+      String telefone = "(43) 99999-9999";
+      String dataNascimento = "25/01/2000";
+
+      a1.setId(id);
+      a1.setNome(nome);
+      a1.setIdade(idade);
+      a1.setEndereco(endereco);
+      a1.setCurso(curso);
+      a1.setTelefone(telefone);
+      a1.setDataNascimento(dataNascimento);
+
+      System.out.println(" ================ ALUNO ================ ");
+      System.out.println(" + ID: " + a1.getId());
+      System.out.println(" + Nome: " + a1.getNome());
+      System.out.println(" + Idade: " + a1.getIdade());
+      System.out.println(" + Endereço: " + a1.getEndereco());
+      System.out.println(" + Curso: " + a1.getCurso());
+      System.out.println(" + Telefone: " + a1.getTelefone());
+      System.out.println(" + Data de Nascimento: " + a1.getDataNascimento());
+      System.out.println(" ======================================== ");
+   }
 }
